65bcbdd5bc2cc6bf2711f778819f30ec56e2edfc
James Browning <james.browning@iohk.io>
2023-10-03 15:13:37 +0100

    Handle empty drepCreds in queryStateForBalancedTx

--- cardano-api/internal/Cardano/Api/Convenience/Query.hs
+++ cardano-api/internal/Cardano/Api/Convenience/Query.hs
@@ -3,50 +3,49 @@
 {-# LANGUAGE TypeFamilies #-}
 
 -- | Convenience query functions
---
 module Cardano.Api.Convenience.Query (
-    QueryConvenienceError(..),
-    determineEra,
-    -- * Simplest query related
-    executeQueryCardanoMode,
-    executeQueryAnyMode,
+  QueryConvenienceError (..),
+  determineEra,
 
-    queryStateForBalancedTx,
-    renderQueryConvenienceError,
-  ) where
+  -- * Simplest query related
+  executeQueryCardanoMode,
+  executeQueryAnyMode,
+  queryStateForBalancedTx,
+  renderQueryConvenienceError,
+) where
 
-import           Cardano.Api.Address
-import           Cardano.Api.Certificate
-import           Cardano.Api.Convenience.Constraints
-import           Cardano.Api.Eras
-import           Cardano.Api.IO
-import           Cardano.Api.IPC
-import           Cardano.Api.IPC.Monad
-import           Cardano.Api.Modes
-import           Cardano.Api.NetworkId
-import           Cardano.Api.ProtocolParameters
-import           Cardano.Api.Query
-import           Cardano.Api.Query.Expr
-import           Cardano.Api.TxBody
-import           Cardano.Api.Utils
-import           Cardano.Api.Value
+import Cardano.Api.Address
+import Cardano.Api.Certificate
+import Cardano.Api.Convenience.Constraints
+import Cardano.Api.Eras
+import Cardano.Api.IO
+import Cardano.Api.IPC
+import Cardano.Api.IPC.Monad
+import Cardano.Api.Modes
+import Cardano.Api.NetworkId
+import Cardano.Api.ProtocolParameters
+import Cardano.Api.Query
+import Cardano.Api.Query.Expr
+import Cardano.Api.TxBody
+import Cardano.Api.Utils
+import Cardano.Api.Value
 
 import qualified Cardano.Ledger.Api as L
 import qualified Cardano.Ledger.Credential as L
-import           Cardano.Ledger.DRepDistr (DRepState (..))
+import Cardano.Ledger.DRepDistr (DRepState (..))
 import qualified Cardano.Ledger.Keys as L
-import           Ouroboros.Consensus.HardFork.Combinator.AcrossEras (EraMismatch (..))
+import Ouroboros.Consensus.HardFork.Combinator.AcrossEras (EraMismatch (..))
 
-import           Control.Monad.Trans (MonadTrans (..))
-import           Control.Monad.Trans.Except (ExceptT (..), runExceptT)
-import           Control.Monad.Trans.Except.Extra (left, onLeft, onNothing)
-import           Data.Function ((&))
-import           Data.Map (Map)
+import Control.Monad.Trans (MonadTrans (..))
+import Control.Monad.Trans.Except (ExceptT (..), runExceptT)
+import Control.Monad.Trans.Except.Extra (left, onLeft, onNothing)
+import Data.Function ((&))
+import Data.Map (Map)
 import qualified Data.Map as Map
-import           Data.Maybe (mapMaybe)
-import           Data.Set (Set)
+import Data.Maybe (mapMaybe)
+import Data.Set (Set)
 import qualified Data.Set as Set
-import           Data.Text (Text)
+import Data.Text (Text)
 
 data QueryConvenienceError
   = AcqFailure AcquiringFailure
@@ -54,69 +53,94 @@ data QueryConvenienceError
   | ByronEraNotSupported
   | EraConsensusModeMismatch !AnyConsensusMode !AnyCardanoEra
   | QceUnsupportedNtcVersion !UnsupportedNtcVersionError
-  deriving Show
+  deriving (Show)
 
 renderQueryConvenienceError :: QueryConvenienceError -> Text
 renderQueryConvenienceError (AcqFailure e) =
   "Acquiring failure: " <> textShow e
 renderQueryConvenienceError (QueryEraMismatch (EraMismatch ledgerEraName' otherEraName')) =
-  "The era of the node and the tx do not match. " <>
-  "The node is running in the " <> ledgerEraName' <>
-  " era, but the transaction is for the " <> otherEraName' <> " era."
+  "The era of the node and the tx do not match. "
+    <> "The node is running in the "
+    <> ledgerEraName'
+    <> " era, but the transaction is for the "
+    <> otherEraName'
+    <> " era."
 renderQueryConvenienceError ByronEraNotSupported =
   "Byron era not supported"
 renderQueryConvenienceError (EraConsensusModeMismatch cMode anyCEra) =
-  "Consensus mode and era mismatch. Consensus mode: " <> textShow cMode <>
-  " Era: " <> textShow anyCEra
+  "Consensus mode and era mismatch. Consensus mode: "
+    <> textShow cMode
+    <> " Era: "
+    <> textShow anyCEra
 renderQueryConvenienceError (QceUnsupportedNtcVersion (UnsupportedNtcVersionError minNtcVersion ntcVersion)) =
-  "Unsupported feature for the node-to-client protocol version.\n" <>
-  "This query requires at least " <> textShow minNtcVersion <> " but the node negotiated " <> textShow ntcVersion <> ".\n" <>
-  "Later node versions support later protocol versions (but development protocol versions are not enabled in the node by default)."
+  "Unsupported feature for the node-to-client protocol version.\n"
+    <> "This query requires at least "
+    <> textShow minNtcVersion
+    <> " but the node negotiated "
+    <> textShow ntcVersion
+    <> ".\n"
+    <> "Later node versions support later protocol versions (but development protocol versions are not enabled in the node by default)."
 
--- | A convenience function to query the relevant information, from
--- the local node, for Cardano.Api.Convenience.Construction.constructBalancedTx
-queryStateForBalancedTx :: ()
-  => CardanoEra era
-  -> [TxIn]
-  -> [Certificate era]
-  -> LocalStateQueryExpr block point (QueryInMode CardanoMode) r IO
-      ( Either
-          QueryConvenienceError
-          ( UTxO era
-          , LedgerProtocolParameters era
-          , EraHistory CardanoMode
-          , SystemStart
-          , Set PoolId
-          , Map StakeCredential Lovelace
-          , Map (L.Credential L.DRepRole L.StandardCrypto) Lovelace))
+{- | A convenience function to query the relevant information, from
+ the local node, for Cardano.Api.Convenience.Construction.constructBalancedTx
+-}
+queryStateForBalancedTx ::
+  () =>
+  CardanoEra era ->
+  [TxIn] ->
+  [Certificate era] ->
+  LocalStateQueryExpr
+    block
+    point
+    (QueryInMode CardanoMode)
+    r
+    IO
+    ( Either
+        QueryConvenienceError
+        ( UTxO era
+        , LedgerProtocolParameters era
+        , EraHistory CardanoMode
+        , SystemStart
+        , Set PoolId
+        , Map StakeCredential Lovelace
+        , Map (L.Credential L.DRepRole L.StandardCrypto) Lovelace
+        )
+    )
 queryStateForBalancedTx era allTxIns certs = runExceptT $ do
-  sbe <- requireShelleyBasedEra era
-    & onNothing (left ByronEraNotSupported)
+  sbe <-
+    requireShelleyBasedEra era
+      & onNothing (left ByronEraNotSupported)
 
-  qeInMode <- pure (toEraInMode era CardanoMode)
-    & onNothing (left (EraConsensusModeMismatch (AnyConsensusMode CardanoMode) (getIsCardanoEraConstraint era $ AnyCardanoEra era)))
+  qeInMode <-
+    pure (toEraInMode era CardanoMode)
+      & onNothing (left (EraConsensusModeMismatch (AnyConsensusMode CardanoMode) (getIsCardanoEraConstraint era $ AnyCardanoEra era)))
 
   let stakeCreds = Set.fromList $ mapMaybe (filterUnRegCreds sbe) certs
-      drepCreds  = Set.fromList $ mapMaybe (filterUnRegDRepCreds sbe) certs
+      drepCreds = Set.fromList $ mapMaybe (filterUnRegDRepCreds sbe) certs
 
   -- Query execution
-  utxo <- lift (queryUtxo qeInMode sbe (QueryUTxOByTxIn (Set.fromList allTxIns)))
-    & onLeft (left . QceUnsupportedNtcVersion)
-    & onLeft (left . QueryEraMismatch)
+  utxo <-
+    lift (queryUtxo qeInMode sbe (QueryUTxOByTxIn (Set.fromList allTxIns)))
+      & onLeft (left . QceUnsupportedNtcVersion)
+      & onLeft (left . QueryEraMismatch)
 
-  pparams <- lift (queryProtocolParameters qeInMode sbe)
-    & onLeft (left . QceUnsupportedNtcVersion)
-    & onLeft (left . QueryEraMismatch)
+  pparams <-
+    lift (queryProtocolParameters qeInMode sbe)
+      & onLeft (left . QceUnsupportedNtcVersion)
+      & onLeft (left . QueryEraMismatch)
 
-  eraHistory <- lift queryEraHistory
-    & onLeft (left . QceUnsupportedNtcVersion)
+  eraHistory <-
+    lift queryEraHistory
+      & onLeft (left . QceUnsupportedNtcVersion)
 
-  systemStart <- lift querySystemStart
-    & onLeft (left . QceUnsupportedNtcVersion)
+  systemStart <-
+    lift querySystemStart
+      & onLeft (left . QceUnsupportedNtcVersion)
 
-  stakePools <- lift (queryStakePools qeInMode sbe)
-    & onLeft (left . QceUnsupportedNtcVersion)
-    & onLeft (left . QueryEraMismatch)
+  stakePools <-
+    lift (queryStakePools qeInMode sbe)
+      & onLeft (left . QceUnsupportedNtcVersion)
+      & onLeft (left . QueryEraMismatch)
 
   stakeDelegDeposits <-
     if null stakeCreds
@@ -127,34 +151,39 @@ queryStateForBalancedTx era allTxIns certs = runExceptT $ do
           & onLeft (left . QueryEraMismatch)
 
   drepDelegDeposits <-
-    Map.map (fromShelleyLovelace . drepDeposit) <$>
-    (lift (queryDRepState qeInMode sbe drepCreds)
-        & onLeft (left . QceUnsupportedNtcVersion)
-        & onLeft (left . QueryEraMismatch))
+    if null drepCreds
+      then pure mempty
+      else
+        Map.map (fromShelleyLovelace . drepDeposit)
+          <$> ( lift (queryDRepState qeInMode sbe drepCreds)
+                  & onLeft (left . QceUnsupportedNtcVersion)
+                  & onLeft (left . QueryEraMismatch)
+              )
 
   pure (utxo, LedgerProtocolParameters pparams, eraHistory, systemStart, stakePools, stakeDelegDeposits, drepDelegDeposits)
 
 -- | Query the node to determine which era it is in.
-determineEra
-  :: ConsensusModeParams mode
-  -> LocalNodeConnectInfo mode
-  -> IO (Either AcquiringFailure AnyCardanoEra)
+determineEra ::
+  ConsensusModeParams mode ->
+  LocalNodeConnectInfo mode ->
+  IO (Either AcquiringFailure AnyCardanoEra)
 determineEra cModeParams localNodeConnInfo =
   case consensusModeOnly cModeParams of
     ByronMode -> return . Right $ AnyCardanoEra ByronEra
     ShelleyMode -> return . Right $ AnyCardanoEra ShelleyEra
     CardanoMode ->
-      queryNodeLocalState localNodeConnInfo Nothing
-        $ QueryCurrentEra CardanoModeIsMultiEra
+      queryNodeLocalState localNodeConnInfo Nothing $
+        QueryCurrentEra CardanoModeIsMultiEra
 
--- | Execute a query against the local node. The local
--- node must be in CardanoMode.
-executeQueryCardanoMode
-  :: SocketPath
-  -> CardanoEra era
-  -> NetworkId
-  -> QueryInMode CardanoMode (Either EraMismatch result)
-  -> IO (Either QueryConvenienceError result)
+{- | Execute a query against the local node. The local
+ node must be in CardanoMode.
+-}
+executeQueryCardanoMode ::
+  SocketPath ->
+  CardanoEra era ->
+  NetworkId ->
+  QueryInMode CardanoMode (Either EraMismatch result) ->
+  IO (Either QueryConvenienceError result)
 executeQueryCardanoMode socketPath era nid q = runExceptT $ do
   let localNodeConnInfo =
         LocalNodeConnectInfo
@@ -166,18 +195,23 @@ executeQueryCardanoMode socketPath era nid q = runExceptT $ do
   ExceptT $ executeQueryAnyMode era localNodeConnInfo q
 
 -- | Execute a query against the local node in any mode.
-executeQueryAnyMode
-  :: forall result era mode. CardanoEra era
-  -> LocalNodeConnectInfo mode
-  -> QueryInMode mode (Either EraMismatch result)
-  -> IO (Either QueryConvenienceError result)
+executeQueryAnyMode ::
+  forall result era mode.
+  CardanoEra era ->
+  LocalNodeConnectInfo mode ->
+  QueryInMode mode (Either EraMismatch result) ->
+  IO (Either QueryConvenienceError result)
 executeQueryAnyMode era localNodeConnInfo q = runExceptT $ do
   let cMode = consensusModeOnly $ localConsensusModeParams localNodeConnInfo
 
-  eraInMode <- pure (toEraInMode era cMode)
-    & onNothing (left $ EraConsensusModeMismatch
-        (AnyConsensusMode CardanoMode)
-        (getIsCardanoEraConstraint era $ AnyCardanoEra era))
+  eraInMode <-
+    pure (toEraInMode era cMode)
+      & onNothing
+        ( left $
+            EraConsensusModeMismatch
+              (AnyConsensusMode CardanoMode)
+              (getIsCardanoEraConstraint era $ AnyCardanoEra era)
+        )
 
   case eraInMode of
     ByronEraInByronMode -> left ByronEraNotSupported
